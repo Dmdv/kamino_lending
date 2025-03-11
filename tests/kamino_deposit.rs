c727e19a1bf2cd2225639b1e54c33fdb0e36dd8b
Dmitry Dyachkov <dima.dyachkov@gmail.com>
2025-03-11 12:36:53 +0700

    tests

--- tests/kamino_deposit.rs
+++ tests/kamino_deposit.rs
@@ -5,56 +5,70 @@ use solana_sdk::{
     pubkey::Pubkey,
 };
 
+use liquidity_lending::instruction::DepositReserveLiquidity;
+
 #[test]
 fn test_deposit_reserve_liquidity_mollusk() {
-    let program_id = Pubkey::new_unique();
+    let program_id = liquidity_lending::ID;
 
     let owner = Pubkey::new_unique();
     let reserve = Pubkey::new_unique();
     let lending_market = Pubkey::new_unique();
     let lending_market_authority = Pubkey::new_unique();
-    let liquidity_mint = Pubkey::new_unique();
-    let liquidity_supply = Pubkey::new_unique();
-    let collateral_mint = Pubkey::new_unique();
+    let reserve_liquidity_mint = Pubkey::new_unique();
+    let reserve_liquidity_supply = Pubkey::new_unique();
+    let reserve_collateral_mint = Pubkey::new_unique();
     let user_source_liquidity = Pubkey::new_unique();
     let user_destination_collateral = Pubkey::new_unique();
     let collateral_token_program = Pubkey::new_unique();
     let liquidity_token_program = Pubkey::new_unique();
     let instruction_sysvar = solana_sdk::sysvar::instructions::id();
+    let kamino_lending_program = Pubkey::new_unique();
 
     let liquidity_amount: u64 = 1_000_000;
-    let instruction_data = liquidity_amount.to_le_bytes().to_vec();
 
-    let instruction = Instruction {
-        program_id,
-        accounts: vec![
-            AccountMeta::new_readonly(owner, true),
-            AccountMeta::new(reserve, false),
-            AccountMeta::new_readonly(lending_market, false),
-            AccountMeta::new_readonly(lending_market_authority, false),
-            AccountMeta::new_readonly(liquidity_mint, false),
-            AccountMeta::new(liquidity_supply, false),
-            AccountMeta::new(collateral_mint, false),
-            AccountMeta::new(user_source_liquidity, false),
-            AccountMeta::new(user_destination_collateral, false),
-            AccountMeta::new_readonly(collateral_token_program, false),
-            AccountMeta::new_readonly(liquidity_token_program, false),
-            AccountMeta::new_readonly(instruction_sysvar, false),
-        ],
-        data: instruction_data,
+    let accounts = liquidity_lending::accounts::KaminoDepositReserveLiquidity {
+        owner,
+        reserve,
+        lending_market,
+        lending_market_authority,
+        reserve_liquidity_mint,
+        reserve_liquidity_supply,
+        reserve_collateral_mint,
+        user_source_liquidity,
+        user_destination_collateral,
+        collateral_token_program,
+        liquidity_token_program,
+        instruction_sysvar_account: instruction_sysvar,
+        kamino_lending_program,
     };
 
-    let accounts = vec![
+    let ix = liquidity_lending::instruction::kamino_deposit_reserve_liquidity(
+        program_id,
+        accounts,
+        liquidity_amount,
+    );
+
+    let mut test_accounts = vec![
         (owner, Account::default()),
         (reserve, Account::default()),
+        (lending_market, Account::default()),
+        (lending_market_authority, Account::default()),
+        (reserve_liquidity_mint, Account::default()),
+        (reserve_liquidity_supply, Account::default()),
+        (reserve_collateral_mint, Account::default()),
         (user_source_liquidity, Account::default()),
         (user_destination_collateral, Account::default()),
+        (collateral_token_program, Account::default()),
+        (liquidity_token_program, Account::default()),
+        (instruction_sysvar, Account::default()),
+        (kamino_lending_program, Account::default()),
     ];
 
-    let mollusk = Mollusk::new(&program_id, "kamino_deposit");
+    let mollusk = Mollusk::new(&program_id, "kamino_deposit_reserve_liquidity");
     mollusk.process_and_validate_instruction(
-        &instruction,
-        &accounts,
+        &ix,
+        &test_accounts,
         &[Check::success()],
     );
 }
